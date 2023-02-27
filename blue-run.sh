1297ead9e398a242ac41fc946751e0fa31e5d0a2
Steve Peters <stephenjonpeters@icloud.com>
2023-02-27 05:13:27 -0700

    cleanup

--- blue-run.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 30000:80 -d stevepeters/blue
--- build.sh
+++
@@ -1,2 +0,0 @@
-docker build . --build-arg DEPLOY_COLOR=blue -t stevepeters/blue
-docker build . --build-arg DEPLOY_COLOR=green -t stevepeters/green
